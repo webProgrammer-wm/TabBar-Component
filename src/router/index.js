85ff6d7af292ed3325a31b7f0325cd15a494f39c
allen <1603657728@qq.com>
2020-05-18 16:49:24 +0800

    '封装tabbar组件'

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,45 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+const Home = () => import('../views/Home/Home')
+const Category = () => import('../views/Category/Category')
+const Cart = () => import('../views/Cart/Cart')
+const Profile = () => import('../views/Profile/Profile')
+
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [
+    {
+        path: '',
+        redirect: '/home'
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/category',
+        name: 'Category',
+        component: Category
+    },
+    {
+        path: '/cart',
+        name: 'Cart',
+        component: Cart
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        component: Profile
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
---
+++ src/views/Cart/Cart.vue
@@ -0,0 +1,16 @@
+<template>
+    <h2>购物车</h2>
+</template>
+
+<script>
+    export default {
+        name: "Cart",
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
